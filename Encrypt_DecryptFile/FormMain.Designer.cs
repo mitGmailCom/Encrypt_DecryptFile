bfd5222af91c8841964cf62b24e34a28255656f8
Admin <Admin@X>
2018-10-09 12:55:39 +0300

    Добавьте файлы проекта.

---
+++ Encrypt_DecryptFile/FormMain.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Encrypt_DecryptFile
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBarProcess = new System.Windows.Forms.ProgressBar();
+            this.btnStartProc = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtnDecrypt = new System.Windows.Forms.RadioButton();
+            this.radioBtnCrypt = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.trackBarSelectNumberSymb = new System.Windows.Forms.TrackBar();
+            this.btnKey = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSelectNumberSymb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBarProcess
+            // 
+            this.progressBarProcess.Location = new System.Drawing.Point(12, 226);
+            this.progressBarProcess.Name = "progressBarProcess";
+            this.progressBarProcess.Size = new System.Drawing.Size(260, 23);
+            this.progressBarProcess.TabIndex = 0;
+            // 
+            // btnStartProc
+            // 
+            this.btnStartProc.Location = new System.Drawing.Point(146, 197);
+            this.btnStartProc.Name = "btnStartProc";
+            this.btnStartProc.Size = new System.Drawing.Size(126, 23);
+            this.btnStartProc.TabIndex = 1;
+            this.btnStartProc.Text = "Go";
+            this.btnStartProc.UseVisualStyleBackColor = true;
+            this.btnStartProc.Click += new System.EventHandler(this.btnStartProc_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtnDecrypt);
+            this.groupBox1.Controls.Add(this.radioBtnCrypt);
+            this.groupBox1.Location = new System.Drawing.Point(0, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(282, 48);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select one";
+            // 
+            // radioBtnDecrypt
+            // 
+            this.radioBtnDecrypt.AutoSize = true;
+            this.radioBtnDecrypt.Location = new System.Drawing.Point(177, 19);
+            this.radioBtnDecrypt.Name = "radioBtnDecrypt";
+            this.radioBtnDecrypt.Size = new System.Drawing.Size(62, 17);
+            this.radioBtnDecrypt.TabIndex = 1;
+            this.radioBtnDecrypt.TabStop = true;
+            this.radioBtnDecrypt.Text = "Decrypt";
+            this.radioBtnDecrypt.UseVisualStyleBackColor = true;
+            this.radioBtnDecrypt.CheckedChanged += new System.EventHandler(this.radioBtnDecrypt_CheckedChanged);
+            // 
+            // radioBtnCrypt
+            // 
+            this.radioBtnCrypt.AutoSize = true;
+            this.radioBtnCrypt.Location = new System.Drawing.Point(45, 19);
+            this.radioBtnCrypt.Name = "radioBtnCrypt";
+            this.radioBtnCrypt.Size = new System.Drawing.Size(49, 17);
+            this.radioBtnCrypt.TabIndex = 0;
+            this.radioBtnCrypt.TabStop = true;
+            this.radioBtnCrypt.Text = "Crypt";
+            this.radioBtnCrypt.UseVisualStyleBackColor = true;
+            this.radioBtnCrypt.CheckedChanged += new System.EventHandler(this.radioBtnCrypt_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Choosed document:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "file";
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(187, 14);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(75, 35);
+            this.btnChooseFile.TabIndex = 6;
+            this.btnChooseFile.Text = "Choose file";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // trackBarSelectNumberSymb
+            // 
+            this.trackBarSelectNumberSymb.Location = new System.Drawing.Point(12, 147);
+            this.trackBarSelectNumberSymb.Minimum = 6;
+            this.trackBarSelectNumberSymb.Name = "trackBarSelectNumberSymb";
+            this.trackBarSelectNumberSymb.Size = new System.Drawing.Size(260, 45);
+            this.trackBarSelectNumberSymb.TabIndex = 7;
+            this.trackBarSelectNumberSymb.Value = 6;
+            this.trackBarSelectNumberSymb.Scroll += new System.EventHandler(this.trackBarSelectNumberSymb_Scroll);
+            // 
+            // btnKey
+            // 
+            this.btnKey.Location = new System.Drawing.Point(12, 197);
+            this.btnKey.Name = "btnKey";
+            this.btnKey.Size = new System.Drawing.Size(126, 23);
+            this.btnKey.TabIndex = 9;
+            this.btnKey.Text = "InputKey";
+            this.btnKey.UseVisualStyleBackColor = true;
+            this.btnKey.Click += new System.EventHandler(this.btnKey_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "val";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Current val:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKey);
+            this.Controls.Add(this.trackBarSelectNumberSymb);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnStartProc);
+            this.Controls.Add(this.progressBarProcess);
+            this.Name = "FormMain";
+            this.Text = "Manage File";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSelectNumberSymb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBarProcess;
+        private System.Windows.Forms.Button btnStartProc;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtnDecrypt;
+        private System.Windows.Forms.RadioButton radioBtnCrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.TrackBar trackBarSelectNumberSymb;
+        private System.Windows.Forms.Button btnKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Encrypt_DecryptFile/FormMain.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encrypt_DecryptFile
+{
+    public partial class FormMain : Form
+    {
+        private string UserKey { get; set; } // key for crypt/decrypt
+        private string Path { get; set; } // path to file, that it will be crypt/decrypt
+        private long countBytesFile = 0; // number bytes in file, that it will be crypt/decrypt
+        Byte[] data; // mass of bytes with data
+        byte[] res1; // mass of bytes with data, that it will be crypt/decrypt
+        Thread thread; // thread to start crypt/decrypt
+        string str = null; // string with data, that it will be crypt/decrypt
+        int countIter = 0; // number of bytes before click button "Cancel"
+
+        private string dat { get; set; }
+        public FormMain()
+        {
+            InitializeComponent();
+            Load += FormMain_Load;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            btnStartProc.Enabled = false;
+        }
+
+
+
+        /// <summary>
+        /// Open InputBox to input key
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnKey_Click(object sender, EventArgs e)
+        {
+            string value = "key123"; // default key
+            InputBoxCreate.InputBox("Key", "Input Key(min length = 6 symbols)", ref value); // invoke InputBox(get key)
+            UserKey = value; // save key
+            MessageBox.Show("Don't forget your key, please!");
+            // check all elements
+            EnableBtnStartCrypt_Decrypt();
+        }
+
+
+        /// <summary>
+        /// Select file to crypt/decrypt
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            DefaultStateButtons();
+            progressBarProcess.Value = 0;
+            label1.Text = "val";
+            trackBarSelectNumberSymb.Value = 6;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            ofd.InitialDirectory = @"C:\Users\Admin\Documents";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Path = ofd.FileName;
+                label4.Text = Path.Substring(Path.LastIndexOf('\\') + 1);
+                countBytesFile = CountBytesInFile();
+                trackBarSelectNumberSymb.Maximum = ConvertToIntCountBytesFile();
+            }
+            // check all elements
+            EnableBtnStartCrypt_Decrypt();
+        }
+
+
+        /// <summary>
+        /// Save data to file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStartProc_Click(object sender, EventArgs e)
+        {
+            if (radioBtnCrypt.Checked)
+            {
+                if (ClickCancel())
+                    return;
+                EnableButtonsInWrite();
+                str = ReedDataFromFile();
+                progressBarProcess.Maximum = str.Length; // set max progressBar
+                thread = new Thread(new ThreadStart(InvokeEncode));
+                thread.Start();
+
+            }
+
+            if (radioBtnDecrypt.Checked)
+            {
+                if (ClickCancel())
+                    return;
+                EnableButtonsInWrite();
+                data = ReedBytesFromFile();
+                progressBarProcess.Maximum = data.Length;
+                thread = new Thread(new ThreadStart(InvokeDecode));
+                thread.Start();
+            }
+        }
+
+        #region For button "Go" (start process)
+        /// <summary>
+        /// Handler for case Decrypt
+        /// </summary>
+        private void InvokeDecode()
+        {
+            Decode(data, UserKey);
+            StartThreadWriteData();
+        }
+        
+        /// <summary>
+        /// Handler for case Crypr
+        /// </summary>
+        private void InvokeEncode()
+        {
+            Encode(str, UserKey);
+            StartThreadWriteData();
+        }
+
+        /// <summary>
+        /// Start process Write to file
+        /// </summary>
+        private void StartThreadWriteData()
+        {
+            if (thread.ThreadState == ThreadState.Running)
+            {
+                Thread threadSecond = new Thread(WriteEncode);
+                threadSecond.Start();
+                threadSecond.Join();
+            }
+        }
+
+        /// <summary>
+        /// Write to file data
+        /// </summary>
+        private void WriteEncode()
+        {
+            string fileName = null;
+            int numberBytesCount = 0;
+            int currentBytesWrite = 0;
+            byte[] tempMasBytes = new byte[1];
+            if (radioBtnCrypt.Checked)
+                fileName = "Encode.txt";
+            if (radioBtnDecrypt.Checked)
+                fileName = "Decode.txt";
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                numberBytesCount = countIter + 1;
+                while (numberBytesCount > 0)
+                {
+                    tempMasBytes[0] = 0;  // clear mass
+                    tempMasBytes[0] = res1[currentBytesWrite]; // set position to write
+                    fs.Write(tempMasBytes, 0, 1); // write byte
+                    numberBytesCount--;
+                    currentBytesWrite++;
+                }
+            }
+            Path = null;
+            UserKey = null;
+        }
+
+        /// <summary>
+        /// Case click Cancel
+        /// </summary>
+        /// <returns>Flag, is cancel click</returns>
+        private bool ClickCancel()
+        {
+            bool isCancel = false;
+            if (btnStartProc.Text == "Cancel")
+            {
+                thread.Abort();
+                WriteEncode();
+                DefaultStateButtons();
+                MessageBox.Show("Data restoret to file!", "Info");
+                progressBarProcess.Maximum = countIter + 1;
+                label4.Text = "-";
+                label1.Text = "val";
+                trackBarSelectNumberSymb.Value = 6;
+                progressBarProcess.Value = 0;
+                Path = null;
+                btnStartProc.Enabled = false;
+                isCancel = true;
+            }
+            return isCancel;
+        }
+        #endregion
+
+
+
+        #region ReedData
+        /// <summary>
+        /// Reed bytes from file
+        /// </summary>
+        /// <returns>String</returns>
+        private string ReedDataFromFile()
+        {
+            data = new Byte[trackBarSelectNumberSymb.Value];
+            FileStream fs = new FileStream(Path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 1024, FileOptions.Asynchronous);
+            fs.BeginRead(data, 0, trackBarSelectNumberSymb.Value, ReadIsComplete, fs);
+            return Encoding.UTF8.GetString(data).Remove(0, 1);
+        }
+
+        /// <summary>
+        /// Reed bytes from file
+        /// </summary>
+        /// <returns>Mass bytes</returns>
+        private byte[] ReedBytesFromFile()
+        {
+            data = new Byte[trackBarSelectNumberSymb.Value];
+            FileStream fs = new FileStream(Path, FileMode.Open, FileAccess.Read, FileShare.Read, 1024, FileOptions.Asynchronous);
+            fs.BeginRead(data, 0, trackBarSelectNumberSymb.Value, ReadIsComplete, fs);
+            return data;
+        }
+
+        /// <summary>
+        /// Method after Filestream (close stream)
+        /// </summary>
+        /// <param name="ar"></param>
+        private void ReadIsComplete(IAsyncResult ar)
+        {
+            FileStream fs = (FileStream)ar.AsyncState;
+            Int32 bytesRead = fs.EndRead(ar);
+            fs.Close();
+        }
+        #endregion
+
+
+
+        /// <summary>
+        /// Get number of bytes in file in type Int
+        /// </summary>
+        /// <returns></returns>
+        private int ConvertToIntCountBytesFile()
+        {
+            int res = 0;
+            bool resToInt = ((Int32.TryParse(countBytesFile.ToString(), out res)));
+            if (resToInt)
+                trackBarSelectNumberSymb.Maximum = Convert.ToInt32(countBytesFile);
+            else
+                res = (int)countBytesFile;
+            return res;
+        }
+
+        /// <summary>
+        /// Calculate number of bytes in file, that user choosed
+        /// </summary>
+        private long CountBytesInFile()
+        {
+            long tempData = 0;
+            if (Path != null || Path != string.Empty)
+                using (FileStream fstream = File.OpenRead(Path))
+                {
+                    //countBytesFile = fstream.Length;
+                    tempData = fstream.Length;
+                }
+            return tempData;
+        }
+
+
+
+        /// <summary>
+        /// Set default state for buttons
+        /// </summary>
+        private void DefaultStateButtons()
+        {
+            btnKey.Text = "InputKey";
+            btnStartProc.Text = "Go";
+        }
+
+        /// <summary>
+        /// Enable buttons during process write data to file
+        /// </summary>
+        private void EnableButtonsInWrite()
+        {
+            //btnKey.Text = "Cancel";
+            btnStartProc.Text = "Cancel";
+        }
+
+        /// <summary>
+        /// Enable button Go
+        /// </summary>
+        private void EnableBtnStartCrypt_Decrypt()
+        {
+            // check all data for process crypt
+            if (!HasEmptyFields())
+                btnStartProc.Enabled = true;
+        }
+
+        /// <summary>
+        /// Check all field that need to crypt
+        /// </summary>
+        /// <returns></returns>
+        private bool HasEmptyFields()
+        {
+            // Fields to check
+            //  - UserKey
+            //  - RadioBtn - no
+            //  - Path
+            //  - NumberSymbReed
+            bool res = false;
+            if (UserKey == string.Empty || UserKey == null)
+                res = true;
+            if (Path == string.Empty || Path == null)
+                res = true;
+            if (trackBarSelectNumberSymb.Value == 0)
+                res = true;
+
+            return res;
+        }
+
+        #region  Changes of state elements
+        private void radioBtnCrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            // check all elements
+            EnableBtnStartCrypt_Decrypt();
+        }
+
+        private void radioBtnDecrypt_CheckedChanged(object sender, EventArgs e)
+        {
+            // check all elements
+            EnableBtnStartCrypt_Decrypt();
+        }
+
+        private void trackBarSelectNumberSymb_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = trackBarSelectNumberSymb.Value.ToString();
+
+            // check all elements
+            EnableBtnStartCrypt_Decrypt();
+        }
+        #endregion
+
+
+
+
+        #region Crypt/Decrypt file
+        /// <summary>
+        /// Crypt file
+        /// </summary>
+        /// <param name="pText"></param>
+        /// <param name="pKey"></param>
+        /// <returns></returns>
+        public void Encode(string pText, string pKey)
+        {
+            byte[] txt = Encoding.Default.GetBytes(pText);
+            byte[] key = Encoding.Default.GetBytes(pKey);
+            res1 = new byte[pText.Length];
+            for (int i = 0; i < txt.Length; i++)
+            {
+                res1[i] = (byte)(txt[i] ^ key[i % key.Length]);
+                progressBarProcess.Invoke(new Action(() => progressBarProcess.Value = i+1));
+                countIter = i;
+            }
+        }
+
+        /// <summary>
+        /// Decrypt file
+        /// </summary>
+        /// <param name="pText">Mass bytes that will decrypt</param>
+        /// <param name="pKey">Key for decrypt</param>
+        /// <returns></returns>
+        public void Decode(byte[] pText, string pKey)
+        {
+            res1 = new byte[pText.Length];
+            byte[] key = Encoding.Default.GetBytes(pKey);
+
+            for (int i = 0; i < pText.Length; i++)
+            {
+                res1[i] = (byte)(pText[i] ^ key[i % key.Length]);
+                progressBarProcess.Invoke(new Action(() => progressBarProcess.Value = i + 1));
+                countIter = i;
+            }
+        }
+        #endregion
+
+    }
+}
